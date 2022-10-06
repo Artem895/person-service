587d9a109bdcc4746be533cdc029797b96876fa1
Artem365 <artiom.utyonoff@yandex.ru>
2022-10-06 14:52:09 +0300

    commit from bash

---
+++ core/src/main/java/liga/medical/personservice/core/PersonServiceApp.java
@@ -0,0 +1,13 @@
+package liga.medical.personservice.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"liga.medical.personservice"})
+public class PersonServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonServiceApp.class, args);
+    }
+}
